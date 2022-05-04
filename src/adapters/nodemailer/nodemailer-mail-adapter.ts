c2be86819c6a9c130b6c4eb6e2867ecf68220bfa
Luciano Victor <lucianocarvallho7@gmail.com>
2022-05-04 14:06:41 -0300

    feat: finalizing the routes, testing and sending email

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a56c37e1f050ce",
+    pass: "4d5d936ed599c4"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Luciano Victor <lucianocarvallho7@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
